6926304007a20866a3a17dac7fba1113bb6587b5
Issaraporn99 <60393062+Issaraporn99@users.noreply.github.com>
2021-05-12 15:54:11 +0700

    เสร็จ

--- getQ2.php
+++ getQ2.php
@@ -9,7 +9,7 @@ $link = mysqli_connect('student.crru.ac.th','601463046','issaraporn@5075','60146
 if (isset($_GET)) {
 	if ($_GET['isAdd'] == 'true') {
 
-		$result = mysqli_query($link, "SELECT `question_id` , `question_name` , `question` , question.`expertise_id` , `expertise_name`, doctorname, `question_date` , answer_name, COUNT( answer_id ) , answer_id
+		$result = mysqli_query($link, "SELECT `question_id` , `question_name` , `question` , question.`expertise_id` , `expertise_name`, doctorname, `question_date` , answer_name, COUNT( answer_id )AS ans , answer_id
 FROM `question`
 LEFT JOIN answer
 USING ( `question_id` )
